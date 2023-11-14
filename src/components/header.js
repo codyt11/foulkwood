407b079e67fd4dfc02254d17a4bbe23731caa23f
unknown <codyt11@hotmail.com>
2023-11-14 01:01:17 -0600

    create-react-app and started header, also set up browser router and render

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,22 @@
-import React from "react"
+import React from "react";
+import { Link } from 'react-router-dom';
+import Shopping from "../assets/shopping-bag.svg";
+import "./header.scss";
 
 const Header = () => (
-    <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+  <div className='header'>
+    <Link className='logo-container' to='#'>
+      <div>logo</div>
     </Link>
     <div className='options'>
-      <Link className='option' to='/shop'>
+      <Link className='option' to='#'>
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className='option' to='#'>
         CONTACT
       </Link>
-      {/* {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )} */}
-      <CartIcon />
+      <img src={Shopping} alt="shopping cart icon" style={{width: '45px', height: '45px', paddingRight: "5px" }}/>
     </div>
-    {hidden ? null : <CartDropdown />}
   </div>
 );
 
