90524569c9dce612e00e134ada168306c93d90db
unknown <codyt11@hotmail.com>
2023-11-13 21:54:47 -0600

    first commit create react app

---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+const Header = () => (
+    <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {/* {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )} */}
+      <CartIcon />
+    </div>
+    {hidden ? null : <CartDropdown />}
+  </div>
+);
+
+export default Header
